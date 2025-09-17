af5057e3098fbacf6aceeb6d80414eba2ff51673
profmauricioneto <mauricio.moreira@unichristus.edu.br>
2025-09-17 09:46:11 -0300

    aula de js - parte 2

--- aulas-javascript/teste.js
+++ aulas-javascript/teste.js
@@ -1,1 +1,30 @@
 console.log("Hello World from NodeJS");
+
+// data type
+// nome = 'mauricio';
+testobj = []
+console.log(typeof nome);
+console.log(typeof 10);
+console.log(typeof 9.5);
+console.log(typeof true);
+console.log(typeof b);
+b=1
+console.log(typeof null);
+console.log(typeof b);
+
+{
+    let nome = 'mauricio';
+}
+// console.log(nome);
+let i;
+for (i = 0; i < 10; i++) {
+    console.log(i);
+}
+console.log('valor de i: ' + i);
+
+let nota = '9';
+console.log(typeof nota);
+nota = parseInt(nota);
+console.log(typeof nota);
+
+
