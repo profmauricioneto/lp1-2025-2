efc72cff022517a2a7ae89f04a24979c6c1972c9
profmauricioneto <mauricio.moreira@unichristus.edu.br>
2025-11-19 19:52:27 -0300

    add: codigo manipulacao da dom

--- aulas-javascript/aula3/manipulacaoDOM/script.js
+++ aulas-javascript/aula3/manipulacaoDOM/script.js
@@ -13,32 +13,51 @@ function mudarTema() {
     }
 } 
 
-// ação de completar ou não uma tarefa
-let items = document.getElementsByClassName('task-item');
-for(let i = 0; i < items.length; i++) {
-    // console.log(items[i].textContent);
-    items[i].addEventListener('click', () => {
-        if (items[i].classList.contains('completed')) {
-            items[i].classList.remove('completed');
-        } else {
-            items[i].classList.add('completed');
-        }
-    });
-}
-
 // capturar entrada de dados
-let botaoSubmit = document.querySelector('[type=submit]');
-console.log(botaoSubmit);
+let botaoSubmit = document.querySelector('[type=submit]'); // referencia do botão
 botaoSubmit.addEventListener('click', (event) => {
     event.preventDefault();
-    let inputText = document.querySelector('#task-input').value;
-    console.log(inputText);
+    let inputText = document.querySelector('#task-input').value; // capturar texto de entrada do usuario
 
+    // criar um elemento li e manipula-lo
     let item = document.createElement('li');
     item.classList.add('task-item');
     item.textContent = inputText;
+    let botao = document.createElement('button');
+    botao.textContent = 'completar';
 
-    console.log(item);
+    // adicionar a funcao de toggle ao botao
+    botao.onclick = () => toggleCompleted(item);
+    item.appendChild(botao);
+
+    // pegar a referencia da lista e adicionar o item criado.
+    let listItem = document.querySelector('#task-list');
+    listItem.appendChild(item);
     
-    document.querySelector('#task-input').value = "";
-})
+    // limpar o campo de entrada de dados
+    // document.querySelector('#task-input').value = "";
+    let form = document.querySelector('#task-form');
+    form.reset();
+});
+
+// ação de completar ou não uma tarefa
+// let items = document.getElementsByClassName('btn-completed');
+// console.log(items);
+// for(let i = 0; i < items.length; i++) {
+//     items[i] = function ('click', () => {    
+//         if (items[i].classList.contains('completed')) {
+//             items[i].classList.remove('completed');
+//         } else {
+//             items[i].classList.add('completed');
+//         }
+//     });
+// }
+
+function toggleCompleted(item) {
+    if (item.classList.contains('completed')) {
+        item.classList.remove('completed');
+    } else {
+        item.classList.add('completed');
+    }
+
+}
